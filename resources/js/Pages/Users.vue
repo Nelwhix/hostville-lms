09135fffa081dc69c9e9494a501c65f3905f7c5f
Nelson Isioma <58360242+Nelwhix@users.noreply.github.com>
2023-03-30 18:49:11 +0100

    ...

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -24,7 +24,7 @@ defineProps({ users: Array })
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg grid grid-cols-3">
+                <div class="bg-white shadow-sm sm:rounded-lg">
                         <table class="table w-full">
                             <!-- head -->
                             <thead>
